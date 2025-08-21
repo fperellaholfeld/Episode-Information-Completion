7039f3e70de3d84f88891ace90c09af6c020449c
fperellaholfeld <fperellaholfeld@gmail.com>
2025-08-21 01:38:02 -0400

    Added to program

--- api/Program.cs
+++ api/Program.cs
@@ -1,9 +1,17 @@
 using api.Data;
+using api.Services.Enrichment;
+using api.Services.RickandMorty;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHttpClient<IRickandMortyClient, RickandMortyClient>(client =>
+{
+    client.BaseAddress = new Uri("https://rickandmortyapi.com/api/");
+    client.Timeout = TimeSpan.FromSeconds(30);
+});
 
 // Add services to the container.
+builder.Services.AddScoped<IEnrichmentService, EnrichmentService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
